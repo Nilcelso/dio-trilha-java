24b3d97deb601dbddb5f03bb39fe57cf504391db
Nilcelso Sobrinho <nilcelsop@gmail.com>
2022-09-07 15:59:47 -0300

    Algumas variabeis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
-public class App {
+public class TiposVariaveis{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos 
+        //String que representa tezto 
+
+        String meuNome = "Nilcelso Sobrinho";
+
+        double salarioMinimo = 2500; // geralmente em moeda se usa double
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto; // neste caso ele ate aceita a variavel numeroCurto 
+        short numeroCurto2 = numeroNormal; // neste caso apresenta um erro pois ele chama um int q a qlq momento pode ser alterado e o short n comportar 
+
+        short numeroCurto3 = (short) numeroNormal; // nesse casoo fazemos a modigicação do numeto 
+
+        
     }
 }
